af897933ef27784783ead137736b29a5d862f23e
YuLim2 <jyulimmm@gmail.com>
2021-11-09 07:09:16 +0000

    자바스크립트

---
+++ BeQuiet/pjtest.js
@@ -0,0 +1,53 @@
+function cssChange() {
+    var x = document.getElementById("abc");
+   x.style.backgroundColor = "blue";
+   if(x.style.background === "blue"){
+       x.style.background = "white";
+   }
+}
+
+
+function cssChange2() {
+   var x = document.getElementById("abc2");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange3() {
+   var x = document.getElementById("abc3");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange4() {
+   var x = document.getElementById("abc4");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange5() {
+   var x = document.getElementById("abc5");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange7() {
+   var x = document.getElementById("abc7");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange8() {
+   var x = document.getElementById("abc8");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange9() {
+   var x = document.getElementById("abc9");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange10() {
+   var x = document.getElementById("abc10");           
+   x.style.backgroundColor = "blue"; 
+}
+
+function cssChange11() {
+   var x = document.getElementById("abc11");           
+   x.style.backgroundColor = "blue"; 
+}
